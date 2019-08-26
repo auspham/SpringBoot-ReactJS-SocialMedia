f1f5c7bc984707a8b5462353dab7e05fdbe21632
Austin <rockmanvnx6@gmail.com>
2019-08-26 15:41:01 +1000

    fix refresh token

--- frontend/todo-app/src/components/todo/AuthenticatedRoute.jsx
+++ frontend/todo-app/src/components/todo/AuthenticatedRoute.jsx
@@ -20,7 +20,7 @@ class AuthenticatedRoute extends Component {
             }
         )
     }
-    componentDidMount() {
+    componentWillMount() {
         this.setupAxiosInterceptors();
     }
     render() {

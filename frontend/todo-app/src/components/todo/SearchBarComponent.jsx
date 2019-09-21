176b3de5e4e46177007569b0856d371617b07c85
Austin <rockmanvnx6@gmail.com>
2019-09-21 22:41:22 +1000

    styling and functionality

--- frontend/todo-app/src/components/todo/SearchBarComponent.jsx
+++ frontend/todo-app/src/components/todo/SearchBarComponent.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import "./search.scss"
 import AccountProfileService from "../../api/todo/AccountProfileService"
+import Socket from '../todo/StartSocket';
+
 class SearchBarComponent extends React.Component {
 
     constructor(props) {
@@ -9,11 +11,22 @@ class SearchBarComponent extends React.Component {
         this.state = {
             users: [],
             search: "",
-            display: false
+            display: false,
+            mouseEnter:  false
         }
     }
 
     componentDidMount() {
+        let stompClient = Socket.connect();
+        stompClient.connect({}, this.onConnected, this.onError);
+        this.getAllUser();
+    }
+
+    onConnected = () => {
+
+    }
+
+    getAllUser = () => {
         AccountProfileService.getAllUser().then(response => {
             this.setState({
                 users: response.data
@@ -22,19 +35,26 @@ class SearchBarComponent extends React.Component {
     }
 
     onFocus = () => {
+        this.getAllUser();
         this.setState({ display: true });
     }
     
     onBlur = () => {
-        this.setState({ display: false });
+        if(!this.state.mouseEnter)
+            this.setState({ display: false });
     }
 
     handleChange = (event) => {
         let content = event.target.value;
         this.setState({ search: content });
     }
-    redirect(){
-        window.location.href = "http://localhost:4200/profile/" + this.props.value;
+
+    onMouseEnter = () => {
+        this.setState({ mouseEnter: true });
+    }
+
+    onMouseLeave = () => {
+        this.setState({ mouseEnter: false });
     }
 
     render() {
@@ -42,8 +62,8 @@ class SearchBarComponent extends React.Component {
             <div className="search-group">
                 <input className="searchBar" onFocus={this.onFocus} onBlur={this.onBlur} value={this.state.search} type="input" placeholder="Search.." onChange={this.handleChange}/>
                 {this.state.display ? 
-                <div className="resultBox">
-                    {this.state.users.filter(user => this.state.search.length > 0 ? user.username.indexOf(this.state.search) > -1 : user).map(each => <div className="userSearch">{each.username}</div>)}
+                <div className="resultBox" onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+                    {this.state.users.filter(user => this.state.search.length > 0 ? user.username.indexOf(this.state.search) > -1 : user).map(each => <a href={"/profile/" + each.username}><div className="userSearch">{each.username}</div></a>)}
                 </div> : null }
             </div>
         )

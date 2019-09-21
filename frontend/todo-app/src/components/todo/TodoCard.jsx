28ade2e7029dc1106be5538aafd1af4395ae2d5b
Austin <rockmanvnx6@gmail.com>
2019-09-21 11:41:59 +1000

    status update for wall

--- frontend/todo-app/src/components/todo/TodoCard.jsx
+++ frontend/todo-app/src/components/todo/TodoCard.jsx
@@ -87,7 +87,7 @@ export default class TodoCard extends Component {
             </div>
             <div className="status-content">
                 {!this.state.show && this.props.todo.description}
-                {this.state.show && <Editable todo={this.props.todo} toggleShow={this.toggleShow} username={this.props.username} refreshTodos={this.props.refreshTodos} content={this.props.todo.description}></Editable>}
+                {this.state.show && <Editable todo={this.props.todo} toggleShow={this.toggleShow} username={this.props.username} refreshTodos={this.props.refreshTodos} content={this.props.todo.description} stompClient={this.props.stompClient}></Editable>}
             </div>
             <div className="comments">
                 <div className="commentHolder" ref="comments">

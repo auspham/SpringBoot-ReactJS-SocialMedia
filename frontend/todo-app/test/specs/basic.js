fff1de59d85ec37af8a58723aaa33697c2b0ffab
Austin <rockmanvnx6@gmail.com>
2019-09-24 09:11:11 +1000

    register test

--- frontend/todo-app/test/specs/basic.js
+++ frontend/todo-app/test/specs/basic.js
@@ -4,10 +4,36 @@ describe('Register test', () => {
     it('It should register a test account', () => {
         browser.url('http://localhost:4200');
         const username = $('input[name="username"]');
-        username.setValue("123");
-        console.log(username.getValue());
-        browser.debug();
-        // username.setValue('test123');
-        // assert.strictEqual(username.getValue(), 'test123');
+        username.setValue("tester01");
+
+        const password = $('input[name="password"]');
+        password.setValue("aA123456");
+
+        const retypepassword = $('input[name="retypepassword"]');
+        retypepassword.setValue(password.getValue());
+
+        const firstName = $('input[name="firstname"]');
+        firstName.setValue("Tester");
+
+        const lastName = $('input[name="lastname"]');
+        lastName.setValue("Nguyen");
+
+        const sNo = $('input[name="studentnumber"]');
+        sNo.setValue("s9999000");
+
+        const sEmail = $('input[name="email"]');
+        sEmail.setValue("s9999000@student.rmit.edu.au");
+
+        const phoneNo = $('input[name="phonenumber"]');
+        phoneNo.setValue("0410999999");
+
+        const submitBtn = $('button[type="submit"]');
+        submitBtn.click();
+
+        assert.strictEqual(0, $$('.checkError').length, "Information must not exist");
+        browser.waitUntil(() => {
+            return browser.getAlertText() === "Register Successful"
+        }, 3000);
+        browser.acceptAlert();
     })
 })

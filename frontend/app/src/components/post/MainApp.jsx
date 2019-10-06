c78bb2ba6470941631e939ca3382f12119a8a2fb
Austin <rockmanvnx6@gmail.com>
2019-10-06 15:48:47 +1100

    fix login bug

--- frontend/app/src/components/post/MainApp.jsx
+++ frontend/app/src/components/post/MainApp.jsx
@@ -58,7 +58,7 @@ class MainApp extends Component {
                     <>
                         <HeaderComponent refreshInfo={this.refreshInfo} handleClick={this.handleClick} handleChange={this.handleChange} />
                         <Switch>
-                            <Route path="/" component={LoginComponent} />
+                            <Route path="/" exact component={LoginComponent} />
                             <Route path="/login" component={LoginComponent} />
                             <AuthenticatedRoute path="/welcome/" component={WelcomeComponent} />
                             <AuthenticatedRoute path="/profile/:username" component={AccountProfile} />

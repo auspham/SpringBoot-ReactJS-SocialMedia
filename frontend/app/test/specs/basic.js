97c8e56e61a70e7d20418e5ae97d6788aa7f6da9
Austin <rockmanvnx6@gmail.com>
2019-10-20 03:28:06 +1100

    Update basic.js

--- frontend/app/test/specs/basic.js
+++ frontend/app/test/specs/basic.js
@@ -32,64 +32,6 @@ casual.define('user_2', function () {
 let user = casual.user;
 let user2 = casual.user_2;
 
-describe('Login and Register negative test', () => {
-    it('It should not register a test account', () => {
-        browser.url('http://localhost:4200');
-        window1 = browser.getWindowHandle();
-
-        const registerBtn = $('button[name="register"]');
-        registerBtn.click();
-        
-        const username = $('input[name="username"]');
-        username.setValue('23');
-
-        const password = $('input[name="password"]');
-        password.setValue('11');
-
-        const retypepassword = $('input[name="retypepassword"]');
-        retypepassword.setValue(password.getValue());
-
-        const firstName = $('input[name="firstname"]');
-        firstName.setValue('123');
-
-        const lastName = $('input[name="lastname"]');
-        lastName.setValue('123123');
-
-        const sNo = $('input[name="studentnumber"]');
-        sNo.setValue('3429599');
-
-        const sEmail = $('input[name="email"]');
-        sEmail.setValue('linh@gmail');
-
-        const phoneNo = $('input[name="phonenumber"]');
-        phoneNo.setValue('123123');
-
-        const submitBtn = $('button[name="register"]');
-        submitBtn.click();
-        
-        let pageUrl = browser.getUrl();
-        assert.notEqual(pageUrl, "http://localhost:4200/welcome/" + username.getValue());
-    })
-
-    it('It should not login with incorrect creditials', () => {
-        const loginBtn = $('p[class="loginLink"]');
-        loginBtn.click();
-
-        const username = $('input[name="username"]');
-        username.setValue("UsernameNotExist");
-
-        const password = $('input[name="password"]');
-        password.setValue("PasswordNotExist");
-
-        const submitBtn = $('button[name="register"]');
-        submitBtn.click();
-
-        let pageUrl = browser.getUrl();
-        assert.notEqual(pageUrl, "http://localhost:4200/welcome/" + username.getValue());
-    })
-
-});
-
 
 describe('Login and Register test', () => {
     it('It should register a test account', () => {

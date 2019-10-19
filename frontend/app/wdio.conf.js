be7ef99fef5fdfebb7ff85c2c05c55e9c482f650
Austin <rockmanvnx6@gmail.com>
2019-10-20 01:49:03 +1100

    Update wdio.conf.js

--- frontend/app/wdio.conf.js
+++ frontend/app/wdio.conf.js
@@ -133,7 +133,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 160000
     },
     //
     // =====
